d5da5b52931d894e5115eec02e0bbde2886ad72e
TofikSaliba <tofik.saliba@gmail.com>
2022-06-01 15:00:46 +0300

    opened repo

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import API from "./api/API";
+
+import "./style.css";
+
+function App() {
+  return <div className="container">App</div>;
+}
+
+export default App;
---
+++ src/api/API.jsx
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://eun1.api.riotgames.com/lol",
+});

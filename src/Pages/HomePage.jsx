f68836dd393acae69e22dbe9ce13e1427850a3e2
TofikSaliba <tofik.saliba@gmail.com>
2022-06-05 15:51:07 +0300

    noth :)

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,6 +3,7 @@ function HomePage() {
     <div>
       HomePage
       <img
+        width="250"
         alt="s"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/1200px-Cat_November_2010-1a.jpg"
       />

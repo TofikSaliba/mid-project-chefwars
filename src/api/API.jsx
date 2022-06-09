c1afe9cc25b1ae9c351d69632e228cb11f032c6f
TofikSaliba <tofik.saliba@gmail.com>
2022-06-09 15:39:02 +0300

    removed unused code commented

--- src/api/API.jsx
+++ src/api/API.jsx
@@ -1,17 +1,5 @@
 import axios from "axios";
 
-// const KEY = "8773019a149c4baa9613ba27b13a12d1";
-
-// export default axios.create({
-//   baseURL: "https://api.spoonacular.com/recipes/complexSearch",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   params: {
-//     apiKey: KEY,
-//   },
-// });
-
 export default axios.create({
   baseURL: "https://www.themealdb.com/api/json/v1/1",
   headers: {

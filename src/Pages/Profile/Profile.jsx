8d357f3ed7bc70cecb09133c17e1aac0b6d4942e
TofikSaliba <tofik.saliba@gmail.com>
2022-06-09 11:40:13 +0300

    from display fix

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -98,7 +98,7 @@ function Profile({ match }) {
         </h4>
 
         <img src={user.img} alt={user.displayName} />
-        {user.location && <div>From: {user.location}</div>}
+        {user.from && <div>From: {user.from}</div>}
         {user.age && <div>Age: {user.age}</div>}
         {user.bio && <p>Bio: {user.bio}</p>}
         {user.id === currentUser.id && (
